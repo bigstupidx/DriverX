e3ff609502d89dbcd5c1f76f852664b18d1facba
Никита Агафонов <taboo3584@gmail.com>
2015-10-05 20:18:12 +0300

    фикс

--- Assets/Resources/Scripts/Probuksovka.cs
+++ Assets/Resources/Scripts/Probuksovka.cs
@@ -1,44 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Probuksovka : MonoBehaviour {
+namespace UnityStandardAssets.Vehicles.Car
+{
 
-    Library library;
-    GameObject probuksovkaParticle;
-    bool isProbuksovka;
-
-    // Use this for initialization
-    void Start()
+    public class Probuksovka : MonoBehaviour
     {
-        library = GameObject.FindObjectOfType<Library>();
-        probuksovkaParticle = transform.GetChild(0).gameObject;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        Rigidbody carRigid = library.car.GetComponent<Rigidbody>();
-        if (library.car.transform.InverseTransformDirection(carRigid.velocity).z < 8 && GetComponent<WheelCollider>().isGrounded && GetComponent<WheelCollider>().motorTorque > 0)
+
+        Library library;
+        GameObject probuksovkaParticle;
+        bool isProbuksovka;
+
+        // Use this for initialization
+        void Start()
         {
-            if (!isProbuksovka)
-            {
-                probuksovkaParticle.SetActive(false);
-                probuksovkaParticle.SetActive(true);
-                probuksovkaParticle.GetComponent<ParticleSystem>().loop = true;
-                probuksovkaParticle.transform.GetChild(0).GetComponent<ParticleSystem>().loop = true;
-                isProbuksovka = true;
-            }
-                    //probuksovkaParticle.SetActive(true);
-        }
-        else
-        {
-            if (isProbuksovka)
-            {
-                probuksovkaParticle.GetComponent<ParticleSystem>().loop = false;
-                probuksovkaParticle.transform.GetChild(0).GetComponent<ParticleSystem>().loop = false;
-                isProbuksovka = false;
-            }
-            //probuksovkaParticle.SetActive(false);
+            library = GameObject.FindObjectOfType<Library>();
+            probuksovkaParticle = transform.GetChild(0).gameObject;
         }
 
+        // Update is called once per frame
+        void Update()
+        {
+            Rigidbody carRigid = library.car.GetComponent<Rigidbody>();
+            if (library.car.GetComponent<CarController>().CurrentSpeed < 12 && GetComponent<WheelCollider>().isGrounded && GetComponent<WheelCollider>().motorTorque > 0)
+            {
+                if (!isProbuksovka)
+                {
+                    probuksovkaParticle.SetActive(false);
+                    probuksovkaParticle.SetActive(true);
+                    probuksovkaParticle.GetComponent<ParticleSystem>().loop = true;
+                    probuksovkaParticle.transform.GetChild(0).GetComponent<ParticleSystem>().loop = true;
+                    isProbuksovka = true;
+                }
+                //probuksovkaParticle.SetActive(true);
+            }
+            else
+            {
+                if (isProbuksovka)
+                {
+                    probuksovkaParticle.GetComponent<ParticleSystem>().loop = false;
+                    probuksovkaParticle.transform.GetChild(0).GetComponent<ParticleSystem>().loop = false;
+                    isProbuksovka = false;
+                }
+                //probuksovkaParticle.SetActive(false);
+            }
+
+        }
     }
 }
